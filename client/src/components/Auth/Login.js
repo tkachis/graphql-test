2485df6e4f305081d7d6ab99ffc180e5801f0f62
Igor Tkachev <tkacheviscoder@gmail.com>
2019-08-28 19:08:16 +0300

    Добавил Google Login Button

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,19 +1,47 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react'
+import { GraphQLClient } from 'graphql-request'
+import { GoogleLogin } from 'react-google-login'
+import { withStyles } from '@material-ui/core/styles'
 // import Typography from "@material-ui/core/Typography";
 
+const ME_QUERY = `
+  {
+    me {
+      _id
+      name
+      email
+      picture
+    }
+  }
+`
+
 const Login = ({ classes }) => {
-  return <div>Login</div>;
-};
+	const onSuccess = async googleUser => {
+		const idToken = googleUser.getAuthResponse().id_token
+		const client = new GraphQLClient('http://localhost:5000/graphql', {
+			headers: { authorization: idToken },
+		})
+		const data = await client.request(ME_QUERY)
+		console.log({ data })
+	}
+
+	return (
+		<GoogleLogin
+			clientId="953815518824-95unf8hivth4a39aftafk3pj4tog204h.apps.googleusercontent.com"
+			onSuccess={onSuccess}
+			isSignedIn={true}
+		/>
+	)
+}
 
 const styles = {
-  root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
-  }
-};
+	root: {
+		height: '100vh',
+		display: 'flex',
+		justifyContent: 'center',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+}
 
-export default withStyles(styles)(Login);
+export default withStyles(styles)(Login)
