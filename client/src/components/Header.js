36ed6fea09bd1ccc5aee02af088d66369cd9805b
Igor Tkachev <tkacheviscoder@gmail.com>
2019-08-28 22:13:19 +0300

    Добавил компонент Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,36 +1,70 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Map from "@material-ui/icons/Map";
-// import Typography from "@material-ui/core/Typography";
+import React, { useContext } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import MapIcon from '@material-ui/icons/Map'
+import Typography from '@material-ui/core/Typography'
 
-const Header = ({ classes }) => {
-  return <div>Header</div>;
-};
+import Context from '../context'
+
+const Header = ({ classes: { root, grow, icon, img } }) => {
+	const {
+		state: { currentUser },
+	} = useContext(Context)
+
+	const { name, picture } = currentUser
+
+	return (
+		<div className={root}>
+			<AppBar position="static">
+				<Toolbar>
+					{/* Title / Logo */}
+					<div className={grow}>
+						<MapIcon className={icon} />
+						<Typography component="h1" variant="h6" color="inherit" noWrap>
+							GeoPins
+						</Typography>
+					</div>
+
+					{/* Current User Info */}
+					{currentUser && (
+						<div className={grow}>
+							<img className={img} src={picture} alt={name} />
+							<Typography variant="h5" color="inherit" noWrap>
+								{name}
+							</Typography>
+						</div>
+					)}
+
+					{/* Signout Button */}
+				</Toolbar>
+			</AppBar>
+		</div>
+	)
+}
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1
-  },
-  grow: {
-    flexGrow: 1,
-    display: "flex",
-    alignItems: "center"
-  },
-  icon: {
-    marginRight: theme.spacing.unit,
-    color: "green",
-    fontSize: 45
-  },
-  mobile: {
-    display: "none"
-  },
-  picture: {
-    height: "50px",
-    borderRadius: "90%",
-    marginRight: theme.spacing.unit * 2
-  }
-});
+	root: {
+		flexGrow: 1,
+	},
+	grow: {
+		flexGrow: 1,
+		display: 'flex',
+		alignItems: 'center',
+	},
+	icon: {
+		marginRight: theme.spacing.unit,
+		color: 'green',
+		fontSize: 45,
+	},
+	mobile: {
+		display: 'none',
+	},
+	img: {
+		height: '50px',
+		borderRadius: '90%',
+		marginRight: theme.spacing.unit * 2,
+	},
+})
 
-export default withStyles(styles)(Header);
+export default withStyles(styles)(Header)
