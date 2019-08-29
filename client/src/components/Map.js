d635231f15a8f6a6eae23fe332ef8f12f05c59df
Igor Tkachev <tkacheviscoder@gmail.com>
2019-08-29 21:36:30 +0300

    Реализовал определение геопозиции пользователя

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
-import ReactMapGL, { NavigationControl } from 'react-map-gl'
+import React, { useState, useEffect } from 'react'
+import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl'
 import { withStyles } from '@material-ui/core/styles'
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+
+import PinIcon from './Pin/PinIcon'
 
 const Map = ({
 	classes: {
@@ -21,6 +23,22 @@ const Map = ({
 		zoom: 13,
 	})
 
+	const [userPosition, setUserPosition] = useState(null)
+
+	useEffect(() => {
+		getUserPosition()
+	}, [])
+
+	const getUserPosition = () => {
+		if ('geolocation' in navigator) {
+			navigator.geolocation.getCurrentPosition(position => {
+				const { latitude, longitude } = position.coords
+				setViewport({ ...viewport, latitude, longitude })
+				setUserPosition({ latitude, longitude })
+			})
+		}
+	}
+
 	return (
 		<div className={root}>
 			<ReactMapGL
@@ -35,6 +53,18 @@ const Map = ({
 				<div className={navigationControl}>
 					<NavigationControl />
 				</div>
+
+				{/* Pin for User's Current Position */}
+				{userPosition && (
+					<Marker
+						latitude={userPosition.latitude}
+						longitude={userPosition.longitude}
+						offsetLeft={-19}
+						offsetTop={-37}
+					>
+						<PinIcon size={40} color="orange" />
+					</Marker>
+				)}
 			</ReactMapGL>
 		</div>
 	)
--- client/src/components/PinIcon.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-// import PlaceTwoTone from "@material-ui/icons/PlaceTwoTone";
-
-export default () => <div>PinIcon</div>;
